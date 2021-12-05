fc3a9d4b55a1002151ac91b57e4d951b06cb358d
gfmota <gabrielfmota134@gmail.com>
2021-12-04 22:16:49 -0300

    Add blue enemy with circular movement
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

--- ProtectTheBody/Assets/Scripts/EnemyBlue.cs
+++ ProtectTheBody/Assets/Scripts/EnemyBlue.cs
@@ -2,32 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyRed : Enemy
+public class EnemyBlue : Enemy
 {
-    private Vector2 secondDirection;
-    private float timeLeft = 0;
-
     public override void Setup()
     {
         body = GetComponent<Rigidbody2D>();
-        Vector2 direction = -transform.position.normalized;
-        body.AddForce(direction * speed, ForceMode2D.Force);
-        secondDirection = new Vector2(direction.y * 0.0015f, -direction.x * 0.0015f);
-        body.AddForce(new Vector2(-direction.y, direction.x), ForceMode2D.Force);
-        body.AddForce(secondDirection * speed, ForceMode2D.Force);
-        timeLeft = 1f;
-
-        enemyAnimations = new string[2] { "enemyRedIdle", "enemyRedDie" };
+        
+        enemyAnimations = new string[2] { "enemyBlueIdle", "enemyBlueDie" };
     }
 
     private void FixedUpdate()
     {
-        if (timeLeft <= 0)
-        {
-            secondDirection = -secondDirection;
-            timeLeft = 2f;
-        }
-        timeLeft -= Time.deltaTime;
-        body.AddForce(secondDirection * speed, ForceMode2D.Impulse);
+        
+        Vector2 direction = -transform.position.normalized;
+
+        Vector2 centripetalForceDirection = (Vector3.zero - transform.position).normalized;
+        // Get centripetal force perpendicular direction
+        Vector2 tangentVelocityDirection = new Vector2(centripetalForceDirection.y, -centripetalForceDirection.x);
+        body.velocity = (tangentVelocityDirection + direction/10) * Time.deltaTime * speed;
     }
 }
